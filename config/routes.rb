4c9cd50dfde70c4a3240b027899809e134cd0ef3
Tomasz Panek <joachimsantana@gmail.com>
2014-09-05 10:27:17 +0200

    add styling for contact form

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,11 @@ Rails.application.routes.draw do
   devise_for :users
   
   resources :posts do
-  	resources :comments, only: [:edit, :update, :create, :destroy]
+    resources :comments, only: [:edit, :update, :create, :destroy]
   end
 
   resources :contacts, only: [:new, :create]
   
-	root 'homes#home'
+  root 'homes#home'
 
 end
