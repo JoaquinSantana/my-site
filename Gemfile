ff0bc1cc234183dabf451b9e23e33ceafff555ae
Tomasz Panek <joachimsantana@gmail.com>
2014-09-05 11:20:47 +0200

    add gem for heroku

--- Gemfile
+++ Gemfile
@@ -34,7 +34,7 @@ group :test do
 end
 
 group :production do
-  gem 'rails_12factor', '0.0.2'
+  gem 'rails_12factor'
 end
 
 gem 'spring',        group: :development
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,30 @@
 module ApplicationHelper
 
+  def flash_messages(opts={})
+    @layout_flash = opts.fetch(:layout_flash) { true }
+    capture do
+      flash.each do |name, msg|
+        case name
+          when "success"
+            "alert-success" # Green
+          when "error"
+           "alert-danger" # Red
+          when "alert"
+           "alert-warning" # Yellow
+          when "notice"
+           "alert-info" # Blue
+          else
+          name.to_s
+        end
+        concat content_tag(:div, msg, id: "flash_#{name}")
+      end
+    end
+  end
+
+  def show_layout_flash?
+    @layout_flash.nil? ? true : @layout_flash
+  end
+
   def bootstrap_class_for(flash_type)
     case flash_type
       when "success"
