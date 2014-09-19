adc41c2bdd56662fe94f9f04800dc2f98c0ec754
Tomasz Panek <joachimsantana@gmail.com>
2014-09-19 11:28:01 +0200

    add ckeditor

--- config/application.rb
+++ config/application.rb
@@ -29,6 +29,7 @@ module MojaStrona
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :pl
+
     
     config.assets.paths << Rails.root.join("app", "assets", "fonts", "images")
     config.assets.precompile += %w(*.png, *.jpg, *.jpeg, *.gif)
